fefc02e4f76f748b5647bc1200dda1d5a535838f
Al <zaudtke@gmail.com>
2017-11-13 14:08:23 -0500

    Add project files.

---
+++ SqlNext/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlNext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
